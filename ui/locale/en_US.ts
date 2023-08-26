d1f995b1cab92746380a28038eeaea01874792cb
evef <eveforest@163.com>
2023-08-27 01:35:59 +0800

    change

--- ui/locale/en_US.ts
+++ ui/locale/en_US.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: EvefyouFE
  * @Date: 2023-08-21 22:29:44
- * @FilePath: \react-evefyou-ui\ui\locale\en-us\index.ts
+ * @FilePath: \react-evefyou-ui\ui\locale\en_US.ts
  * @Description: 
  * Everyone is coming to the world i live in, as i am going to the world lives for you. 人人皆往我世界，我为世界中人人。
  * Copyright (c) 2023 by EvefyouFE/evef, All Rights Reserved. 
